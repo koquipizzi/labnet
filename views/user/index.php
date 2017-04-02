73c479dc373fd4cdf349b971f01435ef2867e971
agussdiaz28 <agussdiaz28@gmail.com>
2017-04-02 17:50:56 -0300

    users arreglo de bugs

--- views/user/index.php
+++ views/user/index.php
@@ -1,7 +1,4 @@
 <?php
-use app\assets\admin\dashboard\DashboardAsset;
-DashboardAsset::register($this);
-
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
@@ -12,11 +9,12 @@ use yii\widgets\Pjax;
 $this->title = Yii::t('app', 'Usuarios');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+
 <section id="page-content">
     <div class="header-content">
-        <h2><i class="fa fa-home"></i>Cipat <span><?= Html::encode($this->title) ?></span></h2>   
+        <h2><i class="fa fa-home"></i>Cipat <span><?= Html::encode($this->title) ?></span></h2>
     </div><!-- /.header-content -->
-    <div class="body-content animated fadeIn" >    
+    <div class="body-content animated fadeIn" >
     <div class="user-index">
         <div class="panel_titulo">
             <div class="panel-heading">
@@ -25,7 +23,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 </div>
                 <div class="pull-right">
                     <?= Html::a(Yii::t('app', 'Create User'), ['create'], ['class' => 'btn btn-success']) ?>
-                </div>   
+                </div>
                 <div class="clearfix"></div>
             </div>
         </div>
@@ -38,28 +36,28 @@ $this->params['breadcrumbs'][] = $this->title;
             'username',
             'email:email',
             ['class' => 'yii\grid\ActionColumn',
-            'template' => '{view}{edit}{delete}',
+            'template' => '{view} {edit} {delete}',
             'buttons' => [
 
             //view button
             'view' => function ($url, $model) {
                 return Html::a('<span class="fa fa-eye "></span>', $url, [
                             'title' => Yii::t('app', 'View'),
-                            'class'=>'btn btn-success btn-xs',                                  
+                            'class'=>'btn btn-success btn-xs',
                 ]);
             },
              'edit' => function ($url, $model) {
                 return Html::a('<span class="fa fa-pencil"></span>', $url, [
                             'title' => Yii::t('app', 'Editar'),
-                            'class'=>'btn btn-info btn-xs',                                  
+                            'class'=>'btn btn-info btn-xs',
                 ]);
             },
              'delete' => function ($url, $model) {
                 return Html::a('<span class="fa fa-trash"></span>', $url, [
                             'title' => Yii::t('app', 'Borrar'),
-                            'class'=>'btn btn-danger btn-xs',                                  
+                            'class'=>'btn btn-danger btn-xs',
                 ]);
-            },        
+            },
         ],
         'urlCreator' => function ($action, $model, $key, $index) {
             if ($action === 'view') {
--- views/user/update.php
+++ views/user/update.php
@@ -1,48 +1,27 @@
 <?php
-use app\assets\admin\dashboard\DashboardAsset;
-DashboardAsset::register($this);
-
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\User */
 
-$this->title = Yii::t('app', 'Update {modelClass}: ', [
-    'modelClass' => 'User',
-]) . $model->id;
+$this->title = Yii::t('app', 'Update {modelClass}: ', ['modelClass' => 'User',]) . $model->id;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Users'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 ?>
 
 <section id="page-content">
-
-    <div class="header-content">
-        <h2><i class="fa fa-home"></i>Cipat <span><?= Html::encode($this->title) ?></span></h2>        
-    </div><!-- /.header-content -->
-    
-    <!-- Start body content -->
-    <div class="body-content animated fadeIn" >
-        <div class="panel rounded shadow">
-            <div class="user-update">
-                <div class="panel-heading">
-                                   <div class="pull-left">
-                                       <h3 class="panel-title"><?= Html::encode($this->title) ?><code></code></h3>
-                                   </div>
-                                   <div class="pull-right">
-                                       <button class="btn btn-sm" data-action="collapse" data-container="body" data-toggle="tooltip" data-placement="top" data-title="Collapse"><i class="fa fa-angle-up"></i></button>
-                                       <button class="btn btn-sm" data-action="remove" data-container="body" data-toggle="tooltip" data-placement="top" data-title="Remove"><i class="fa fa-times"></i></button>
-                                   </div>
-                                   <div class="clearfix"></div>
-                </div><!-- /.panel-heading -->                 
-                
-
+    <div class="box box-info">
+                <div class="box-header with-border">
+                 <div class="pull-right">
+                        <?= Html::a('<i class="fa fa-arrow-left"></i> Volver', ['user/index'],['class'=>'btn btn-primary']) ?>
+                    </div>
+                  <h3 class="box-title"><?= Html::encode($this->title) ?></h3>
+                </div>
                 <?= $this->render('_form', [
-                    'model' => $model,
-                ]) ?>
+                        'model' => $model,
 
-            </div>
-        </div>
+                    ]) ?>
     </div>
+
 </section>
-            

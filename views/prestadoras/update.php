b49974c42f7ce8647fe55b0b63aef6d01d01b835
agussdiaz28 <agussdiaz28@gmail.com>
2017-04-05 20:10:16 -0300

    prestadora fix

--- views/prestadoras/update.php
+++ views/prestadoras/update.php
@@ -15,9 +15,9 @@ $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 <section id="page-content">
 
     <div class="header-content">
-        <h2><i class="fa fa-home"></i>Cipat <span><?= Html::encode($this->title) ?></span></h2>        
+        <h2><i class="fa fa-home"></i>Cipat <span><?= Html::encode($this->title) ?></span></h2>
     </div><!-- /.header-content -->
-    
+
     <!-- Start body content -->
     <div class="body-content animated fadeIn" >
         <div class="panel rounded shadow">
@@ -26,13 +26,9 @@ $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
                                    <div class="pull-left">
                                        <h3 class="panel-title"><?= Html::encode($this->title) ?><code></code></h3>
                                    </div>
-                                   <div class="pull-right">
-                                       <button class="btn btn-sm" data-action="collapse" data-container="body" data-toggle="tooltip" data-placement="top" data-title="Collapse"><i class="fa fa-angle-up"></i></button>
-                                       <button class="btn btn-sm" data-action="remove" data-container="body" data-toggle="tooltip" data-placement="top" data-title="Remove"><i class="fa fa-times"></i></button>
-                                   </div>
                                    <div class="clearfix"></div>
-                </div><!-- /.panel-heading -->                 
-                
+                </div><!-- /.panel-heading -->
+
 
                 <?= $this->render('_form', [
                     'model' => $model,
@@ -42,4 +38,3 @@ $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
         </div>
     </div>
 </section>
-            

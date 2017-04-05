69b4ecf46439b7f9496f9dbc1a940b5cd1971617
koqui <koquipizzi@gmail.com>
2017-04-05 16:59:08 -0300

    vistas

--- views/informe/update.php
+++ views/informe/update.php
@@ -155,6 +155,7 @@ $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 </section>
 <?php
 
+
 $this->registerJsFile('@web/assets/admin/js/cipat_modal_informe.js', ['depends' => [yii\web\AssetBundle::className()]]);
 
 

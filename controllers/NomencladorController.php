30ef07af0d8a204a239180b367b2db4c0740c31f
Alejandra Pizzi <alejandra@qwavee.com>
2017-10-04 16:44:10 -0300

    arreglos en bug Nomenclador

--- controllers/NomencladorController.php
+++ controllers/NomencladorController.php
@@ -87,7 +87,7 @@ class NomencladorController extends Controller
         $model = $this->findModel($id);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return;// $this->redirect(['view', 'id' => $model->id]);
+            $this->redirect(['view', 'id' => $model->id]);
         } else {
             return $this->render('update', [
                 'model' => $model,

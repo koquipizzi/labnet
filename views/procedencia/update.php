29f1d3f07bc8d3bacc09f7a30927317fb97f7b91
Alejandra Pizzi <alejandra@qwavee.com>
2017-10-04 17:06:16 -0300

    filtrado de nomenclador por prestadora LAB-169

--- views/procedencia/update.php
+++ views/procedencia/update.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Procedencia */
 
-$this->title = Yii::t('app', 'Update {modelClass}: ', ['modelClass' => 'Procedencia',]) . $model->id;
+$this->title = Yii::t('app', 'Update {modelClass}: ', ['modelClass' => 'Procedencia',]) . $model->descripcion;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Procedencias'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');

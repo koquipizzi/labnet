288819a54ad9a24bf3c2f7b88a72cd8ca44e8ff3
Alejandra Pizzi <alejandra@qwavee.com>
2017-10-17 15:39:27 -0300

    modificaciones menu

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -12,7 +12,7 @@ Yii::$app->name = "LabNET";
 
     <nav class="navbar navbar-static-top" role="navigation">
 
-        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
             <span class="sr-only">Toggle navigation</span>
         </a>
 

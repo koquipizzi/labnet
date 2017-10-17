350a60302973a844847f7325e8c0a0af62ad6f8c
superq <franco.e.aller@gmail.com>
2017-10-17 11:56:33 -0300

    fix protocolo assignados#2

--- models/ProtocoloSearch.php
+++ models/ProtocoloSearch.php
@@ -795,7 +795,7 @@ class ProtocoloSearch extends Protocolo
 
 
 
-
+  
 
       public function search_asignados($id=null, $params=NULL)
     {
@@ -827,7 +827,7 @@ class ProtocoloSearch extends Protocolo
                     JOIN Paciente ON (Paciente_prestadora.Paciente_id = Paciente.id)
 					JOIN view_informe_ult_workflow ON (Informe.id = view_informe_ult_workflow.Informe_id)
 					JOIn Workflow on view_informe_ult_workflow.id = Workflow.id 
-					WHERE Workflow.Estado_id =3 or Workflow.Estado_id =4      
+					WHERE Workflow.Estado_id >=3 and Workflow.Estado_id <=4      
                     AND Workflow.Responsable_id = ".$loggedUserId;
         
         if (isset($params['ProtocoloSearch']['nro_secuencia']) && ($params['ProtocoloSearch']['nro_secuencia'] <> "") )

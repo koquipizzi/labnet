f0a369c60c334b11422a39649c2d2865a59dace3
superq <franco.e.aller@gmail.com>
2017-10-23 15:27:11 -0300

    mail informes con estilos

--- views/informe/_print_biopsia_mail.php
+++ views/informe/_print_biopsia_mail.php
@@ -2,42 +2,78 @@
 use app\models\Leyenda;
 use yii\helpers\Html;
 ?>
+
 <html>
 <body> 
 
+<style>
+
+body{
+    font-family: Arial;
+    font-size: 12px; 
+}
+
+.pap_labels{
+     font-weight: bold; 
+     padding-bottom: 10px;
+         
+}
+.pap_desc{
+    font-size: 12px;
+    font-style: italic;
+    padding-bottom: 15px; 
+    padding-left: 50px; 
+}
+
+.pap_desc_cito {
+    padding-left:50px;
+}
+.pap_labels_cito{
+    font-style: normal;
+}
+
+table.pap_desc {
+    padding-left: 0px;
+}
+
+
+.header_pap{
+    font-size: 13px;
+}
+</style>
 
 <div class="pagina">
     <div class="header_pap">
         <table>
             <tr>
                 <td>
-                    
+                   
                 </td>
             </tr>
             <tr>
-                <td width="400px" style="padding-left: 20px; padding-top: 180px;float: left;">
+                <td width="400px"  style="padding-top: 180px; padding-left: 20px; float: left;">
                     <table class="header_pap">
                         <tr>
                            <td style="white-space:nowrap; width: 3cm; font-weight: bold;">PACIENTE </td><td><?php echo $modelp->pacienteTexto; ?> </td>
                         </tr>
                         <tr>
-                            <td style="white-space:nowrap; width: 3cm; font-weight: bold;">DOCUMENTO </td><td><?php echo $modelp->pacienteDoc; ?> </td>
+                            <td style="white-space:nowrap; width: 3cm; font-weight: bold;">DOCUMENTO </td><td><?php echo $modelp->pacienteDoc ? $modelp->pacienteDoc : "Sin datos" ?> </td>
                         </tr>
                         <tr>
-                            <td style="white-space:nowrap; width: 3cm; font-weight: bold;">EDAD </td><td><?php echo $model->edad; ?> </td>
+                            <td style="white-space:nowrap; width: 3cm; font-weight: bold;">EDAD </td><td><?php echo $model->edad == 0 ? "Sin datos" : $model->edad; ?> </td>
                         </tr>
                         <tr>
-                            <td style="white-space:nowrap; width: 3cm; font-weight: bold;">COBERTURA </td><td><?php echo $modelp->cobertura; ?> </td>
+                            <td style="white-space:nowrap; width: 3cm; font-weight: bold;">COBERTURA </td><td><?php echo $modelp->cobertura; ?>  </td>
                         </tr>
                     </table>
                 </td>
-                <td width="400px" style="padding-top: 180px; margin-left: 20px; float: right;">
+                <td width="400px" style="margin-left: 20px;padding-top: 180px; float: right;">
                     <table class="header_pap">
                         <tr>
                             <td style="white-space:nowrap; width: 3cm; font-weight: bold;">PROTOCOLO</td><td><?php echo $modelp->codigo; ?> </td>
                         </tr>
                         <tr>
-                            <td style="white-space:nowrap; width: 3cm; font-weight: bold;">FECHA</td><td><?php echo $modelp->fechaEntrega;  ?> </td>
+                            <td style="white-space:nowrap; width: 3cm; font-weight: bold;">FECHA</td><td><?php echo $modelp->fechaEntrega;  ?>  </td>
                         </tr>
                         <tr>
                             <td style="white-space:nowrap; width: 3cm; font-weight: bold;">MÉDICO </td><td><?php echo $modelp->medico->nombre; ?> </td>
@@ -51,7 +87,9 @@ use yii\helpers\Html;
         </table>
     </div>
     <hr>
-    <h5 style="font-size:13px; text-align: center; margin-left: 20px; font-weight: bold; text-decoration: underline">INFORME INMUNOHISTOQUÍMICO</h5>
+    <h5 style="font-size:13px; text-align: center; margin-left: 20px; font-weight: bold; text-decoration: underline">
+        INFORME ANATOMOPATÓLOGICO
+    </h5>
     <div class="informe">
         <div class="pap_labels">
             MATERIAL
@@ -65,37 +103,37 @@ use yii\helpers\Html;
             MACROSCOPÍA
         </div>
         <div class="pap_desc">
-            <?php echo nl2br($model->macroscopia)  ?>
+            <?php echo nl2br($model->macroscopia); ?>
         </div>
         <div class="pap_labels">
             MICROSCOPÍA
         </div>
         <div class="pap_desc">
-            <?php echo nl2br($model->microscopia) ?>
+            <?php echo nl2br($model->microscopia); ?>
         </div>
         <div class="pap_labels">
             DIAGNÓSTICO
         </div> 
         <div class="pap_desc">
-            <?php echo nl2br($model->diagnostico)  ?>
+            <?php echo nl2br($model->diagnostico);  ?>
         </div>
         <div class="pap_labels">
             OBSERVACIONES
         </div>
         <div class="pap_desc">
-            <?php echo nl2br($model->observaciones)  ?>
+            <?php echo nl2br($model->observaciones);  ?>
         </div>
-    </div>  
-</div>
+    </div>
     <div style="position: fixed; margin-right: 35px; text-align: right;">
         <img src="<?php echo Yii::getAlias('@webroot').'/images/firma/firma.jpg'; ?>" width="120"  \/>  
     </div>
 
-
+</div>
 
 <div class="footer" style="position: fixed; bottom: -5px; text-align: center; font-size: 11px; width:100%">
     INFORMACIÓN CONFIDENCIAL - SECRETO MÉDICO - ALCANCES DEL ARTÍCULO 156 DEL CÓDIGO PENAL
 </div>
-    
-</body> 
+
+</body>    
 </html>
+ 
--- views/informe/_print_mole_mail.php
+++ views/informe/_print_mole_mail.php
@@ -7,11 +7,10 @@ use yii\helpers\Html;
 
 
 <div class="pagina">
-    <div class="header_pap">       
+    <div class="header_pap">
         <table>
-            <tr height="400" style="margin-top:100px;">
-                <td>                    
-                     
+            <tr>
+                <td>
                 </td>
             </tr>
             <tr>
@@ -27,7 +26,7 @@ use yii\helpers\Html;
                             <td style="white-space:nowrap; width: 3cm; font-weight: bold;">EDAD </td><td><?php echo $model->edad; ?> </td>
                         </tr>
                         <tr>
-                            <td style="white-space:nowrap; width: 3cm; font-weight: bold;">COBERTURA </td><td><?php echo $modelp->cobertura; ?></td>
+                            <td style="white-space:nowrap; width: 3cm; font-weight: bold;">COBERTURA </td><td><?php echo $modelp->cobertura; ?> </td>
                         </tr>
                     </table>
                 </td>
@@ -37,7 +36,7 @@ use yii\helpers\Html;
                             <td style="white-space:nowrap; width: 3cm; font-weight: bold;">PROTOCOLO</td><td><?php echo $modelp->codigo; ?> </td>
                         </tr>
                         <tr>
-                            <td style="white-space:nowrap; width: 3cm; font-weight: bold;">FECHA</td><td><?php echo $modelp->fechaEntrega; ?> </td>
+                            <td style="white-space:nowrap; width: 3cm; font-weight: bold;">FECHA</td><td><?php echo $modelp->fechaEntrega;  ?> </td>
                         </tr>
                         <tr>
                             <td style="white-space:nowrap; width: 3cm; font-weight: bold;">MÉDICO </td><td><?php echo $modelp->medico->nombre; ?> </td>
@@ -52,41 +51,28 @@ use yii\helpers\Html;
     </div>
     <hr>
     <h5 style="font-size: 13px; text-align: center; margin-left: 20px; font-weight: bold; text-decoration: underline">
-        <?php echo $model->titulo; ?>
+        BIOLOGÍA MOLECULAR - HPV DNA TEST
     </h5>
-    <div class="informe">
+    <div class="informe">        
         <div class="pap_labels">
             MATERIAL
         </div>    
         <div class="pap_desc">
             <?php echo nl2br($model->material); ?>
+            <br><br>
+            <?php echo nl2br($model->tecnica); ?>
         </div>
         <div class="pap_labels">
-            TECNICA
+            METODO
         </div>    
         <div class="pap_desc">
-            <?php echo nl2br($model->tecnica); ?>
+            <?php echo nl2br($model->macroscopia); ?>
         </div>        
-        <div class="pap_labels">
-            CITOLOGÍA HORMONAL
-        </div>  
+        <div class="pap_labels">            
+            RESULTADO
+        </div> 
         <div class="pap_desc">
-            <table class="pap_desc">
-                <tr><td class="pap_labels_cito">CALIDAD DE MUESTRA</td> <td  class="pap_desc_cito"><?php echo $model->calidad ? Leyenda::findOne(['id' => $model->calidad ])->texto : ""  ?></td></tr>
-                <tr><td class="pap_labels_cito">ASPECTO</td>            <td  class="pap_desc_cito"><?php echo $model->aspecto ? Leyenda::findOne(['id' => $model->aspecto ])->texto : ""  ?></td></tr>
-                <tr><td class="pap_labels_cito">FLORA</td>              <td  class="pap_desc_cito"><?php echo $model->flora ? Leyenda::findOne(['id' => $model->flora ])->texto : ""  ?></td></tr>
-                <tr><td class="pap_labels_cito">LEUCOCITOS</td>         <td  class="pap_desc_cito"><?php echo $model->leucositos ? Leyenda::findOne(['categoria' => 'LH','codigo'=> $model->leucositos])->texto : ""?></td></tr>
-                <tr><td class="pap_labels_cito">HEMATIES</td>           <td  class="pap_desc_cito"><?php echo $model->hematies ? Leyenda::findOne(['categoria' => 'LH','codigo'=> $model->hematies])->texto : "" ?></td></tr>
-                <tr><td class="pap_labels_cito">OTROS ELEMENTOS</td>    <td  class="pap_desc_cito"><?php echo $model->otros ? Leyenda::findOne(['id' => $model->otros ])->texto : ""  ?></td></tr>
-                <tr><td class="pap_labels_cito">MICROORGANISMOS</td>    <td  class="pap_desc_cito"><?php echo $model->microorganismos ? Leyenda::findOne(['id' => $model->microorganismos ])->texto : ""  ?></td></tr>
-            </table>
-            
-        </div>
-        <div class="pap_labels">
-            CITOLOGÍA ONCOLÓGICA
-        </div>
-        <div class="pap_desc">
-            <?php echo nl2br($model->citologia); ?>
+            <?php echo nl2br($model->microscopia);  ?>
         </div>
         <div class="pap_labels">
             DIAGNÓSTICO
@@ -106,8 +92,9 @@ use yii\helpers\Html;
     </div>
 </div>
 
+
 <div class="footer" style="position: fixed; bottom: -5px; text-align: center; font-size: 11px; width:100%">
     INFORMACIÓN CONFIDENCIAL - SECRETO MÉDICO - ALCANCES DEL ARTÍCULO 156 DEL CÓDIGO PENAL
 </div>
-</body>     
-</html>
+</body> 
+</html>      

8618137c58ea3978109956033e5f2bc37de35c94
Alejandra Pizzi <alejandra@qwavee.com>
2018-04-27 08:32:59 -0300

    sin vardump

--- controllers/InformeController.php
+++ controllers/InformeController.php
@@ -299,7 +299,6 @@ class InformeController extends Controller {
                 }
             }
             //obtine el utlimo estado 
-			//var_dump($model->id);
             $ultimoEstado = Workflow::find ( 'id' )->where ( [
                             'Informe_id' => $model->id
                                 ] )->orderBy ( [

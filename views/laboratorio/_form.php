c08e009a9e5c6e67f9e07c3fe5be9ddb19076488
agussdiaz28 <agussdiaz28@gmail.com>
2018-05-07 10:41:01 -0300

    fix de modelado de la pagina

--- views/laboratorio/_form.php
+++ views/laboratorio/_form.php
@@ -11,17 +11,8 @@ use yii\widgets\Pjax;
 ?>
   <?php
   
-  $js = '
-    
-    $("#idFile").on("fileuploaded", function(event) {
-        $.pjax.reload({container:"#galeriaL"});
-    });
-    
-    $("#idFile2").on("fileuploaded", function(event) {
-        $.pjax.reload({container:"#galeriaFD"});
-    });
-
-    $(document).on("pjax:success", function() {
+      $js = '
+        
         $("#idFile").on("fileuploaded", function(event) {
             $.pjax.reload({container:"#galeriaL"});
         });
@@ -29,21 +20,30 @@ use yii\widgets\Pjax;
         $("#idFile2").on("fileuploaded", function(event) {
             $.pjax.reload({container:"#galeriaFD"});
         });
-    });
     
-    ';
+        $(document).on("pjax:success", function() {
+            $("#idFile").on("fileuploaded", function(event) {
+                $.pjax.reload({container:"#galeriaL"});
+            });
+            
+            $("#idFile2").on("fileuploaded", function(event) {
+                $.pjax.reload({container:"#galeriaFD"});
+            });
+        });
+        
+      ';
  
     $this->registerJs($js);
   ?>
 
-    <?php $form = ActiveForm::begin([
+  <?php $form = ActiveForm::begin([
         'options' => [
             'class' => 'form-horizontal mt-10',
             'id' => 'create-localidad-form',
             'options'=>['enctype'=>'multipart/form-data']
             
          ]
-    ]); ?>
+  ]); ?>
 
     <?= $form->field($model, 'nombre', ['template' => "{label}
             <div class='col-md-7'>{input}</div>
@@ -106,106 +106,105 @@ use yii\widgets\Pjax;
     <div class="col-md-7  ">
         <?= $form->field($model, 'leyenda_informe')->widget(\yii\redactor\widgets\Redactor::className()) ?>
     </div>
-
-    <div class="col-md-3  control-label"><p>Cargar Logo</p></div>
-    <div class="col-md-7  ">
-        <?php
-        echo FileInput::widget([
-                        'model' => $model,
-                        'attribute' => 'files[]',
-                        'options'=>[
-                                        'multiple'=>false,
-                                        'accept'=>'image/*',
-                                        'id'=>"idFile",
+    <div class="row">
+        <div class="col-md-3  control-label"><p>Cargar Logo</p></div>
+        <div class="col-md-7  ">
+            <?php
+                echo FileInput::widget([
+                    'model' => $model,
+                    'attribute' => 'files[]',
+                    'options'=>[
+                        'multiple'=>false,
+                        'accept'=>'image/*',
+                        'id'=>"idFile",
+                    ],
+                    'pluginOptions' => [
+                        'uploadUrl' => Url::to(['/laboratorio/logo']),
+                        'layoutTemplates'=>[
+                            'preview'=>'<div class="file-preview {class}">' .
+                                '    {close}' .
+                                '    <div class="{dropClass}">' .
+                                '    <div class="file-preview-thumbnails">' .
+                                '    </div>'.
+                                '    <div class="clearfix"></div>' .
+                                '    <div class="file-preview-status text-center text-success"></div>' .
+                                '    </div>' .
+                                '</div>',
                         ],
-                        'pluginOptions' => [
-                                        'uploadUrl' => Url::to(['/laboratorio/logo']),
-                                        'layoutTemplates'=>[
-                                                'preview'=>'<div class="file-preview {class}">' .
-                                                '    {close}' .
-                                                '    <div class="{dropClass}">' .
-                                                '    <div class="file-preview-thumbnails">' .
-                                                '    </div>'.
-                                                '    <div class="clearfix"></div>' .
-                                                '    <div class="file-preview-status text-center text-success"></div>' .
-                                                '    </div>' .
-                                                '</div>',
-                                             ],
+                    ],
+                ]);
+            ?>
+
+            <div class="row pull-right">
+                <?php Pjax::begin(['id' => 'galeriaL', 'enablePushState' => TRUE]); ?>
+                <br>
+                <div class="content-galeria">
+                
+                    <?php //Muestra el logo
+                        if (!empty( $model->web_path)){
+                            echo $this->render('galeria', [
+                                'path' => $model->web_path
+                            ]);
+                        }
+                    ?>
+                </div>
+                <?php Pjax::end(); ?>
+            </div>
+        </div>
+    </div>
+    <hr style="border-top:0;">
+    <div class="row">
+        <div class="col-md-3  control-label" style="margin-top:5px; "><p>Firma Digital</p></div>
+        <div class="col-md-7">
+            <?php
+                echo FileInput::widget([
+                    'model' => $model,
+                    'attribute' => 'files[]',
+                    'options'=>[
+                        'multiple'=>false,
+                        'accept'=>'image/*',
+                        'id'=>"idFile2",
+                
+                    ],
+                    'pluginOptions' => [
+                        'uploadUrl' => Url::to(['/laboratorio/firmadigital']),
+                        'layoutTemplates'=>[
+                            'preview'=>'<div class="file-preview {class}">' .
+                                '    {close}' .
+                                '    <div class="{dropClass}">' .
+                                '    <div class="file-preview-thumbnails">' .
+                                '    </div>'.
+                                '    <div class="clearfix"></div>' .
+                                '    <div class="file-preview-status text-center text-success"></div>' .
+                                '    </div>' .
+                                '</div>',
                         ],
-                  ]);
-        ?>
-
-
-        <div class="row pull-right">
-            <?php Pjax::begin(['id' => 'galeriaL', 'enablePushState' => TRUE]); ?>
-            <br>
-            <div class="content-galeria">
+                    ],
             
-                <?php //Muestra el logo
-                    if (!empty( $model->web_path)){
-                        echo $this->render('galeria', [
-                            'path' => $model->web_path
-                        ]);
-                    }
-                ?>
+                ]);
+            ?>
+
+            <div class="row pull-right">
+                <?php Pjax::begin(['id' => 'galeriaFD', 'enablePushState' => TRUE]); ?>
+                <div class="content-galeria">
+                    <?php //muestra la firma
+                        if (!empty($model->web_path_firma)){
+                            echo $this->render('galeria', [
+                                'path' => $model->web_path_firma
+                            ]);
+                        }
+                    ?>
+                </div>
+                <?php Pjax::end(); ?>
             </div>
-            <?php Pjax::end(); ?>
-        </div>
-        
-    </div>
-
-    <div class="col-md-3  control-label" style="margin-top:5px; "><p>Firma Digital</p></div>     
-    <div class="col-md-7">
-        <hr>
-        <?php 
-        echo FileInput::widget([
-                        'model' => $model,
-                        'attribute' => 'files[]',
-                        'options'=>[
-                                        'multiple'=>false,
-                                        'accept'=>'image/*',
-                                        'id'=>"idFile2",
-
-                        ],
-                        'pluginOptions' => [
-                                        'uploadUrl' => Url::to(['/laboratorio/firmadigital']),
-                                        'layoutTemplates'=>[
-                                                'preview'=>'<div class="file-preview {class}">' .
-                                                '    {close}' .
-                                                '    <div class="{dropClass}">' .
-                                                '    <div class="file-preview-thumbnails">' .
-                                                '    </div>'.
-                                                '    <div class="clearfix"></div>' .
-                                                '    <div class="file-preview-status text-center text-success"></div>' .
-                                                '    </div>' .
-                                                '</div>',
-                                             ],										
-                        ],
-
-                  ]);
-        ?>
-        
-        <div class="row pull-right">
-            <?php Pjax::begin(['id' => 'galeriaFD', 'enablePushState' => TRUE]); ?>
-            <div class="content-galeria">
-                <?php //muestra la firma
-                    if (!empty($model->web_path_firma)){
-                        echo $this->render('galeria', [
-                            'path' => $model->web_path_firma
-                        ]);
-                    }
-                ?>
-            </div>
-            <?php Pjax::end(); ?>
         </div>
     </div>
-    
     <div class="box-footer" >
         <div class="pull-right box-tools">
             <hr style="border-top:0;">
             <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
         <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
-        </div>         
+        </div>
     </div>
 
     <?php ActiveForm::end(); ?>

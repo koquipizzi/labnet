a4683e4795d76e01a402b12c25444debfc700519
agussdiaz28 <agussdiaz28@gmail.com>
2018-05-15 12:14:12 -0300

    mejoras en search

--- views/site/search.php
+++ views/site/search.php
@@ -11,6 +11,19 @@
 <?php
     $javaScript = <<<JS
     
+
+    $.fn.pressEnter = function(fn) {
+        return this.each(function() {
+                $(this).bind('enterPress', fn);
+                    $(this).keyup(function(e){
+                        if(e.keyCode == 13)
+                        {
+                          $(this).trigger("enterPress");
+                        }
+                    })
+            });
+    };
+    
     $('#entidad-select').on('change',function() {
         if ($('#entidad-select').val() === '3' ){
             $('.protocolo-search').show();
@@ -18,8 +31,8 @@
              $('.protocolo-search').hide();
         }
     });
-
-    $('#search-filtro').on('click',function() {
+    
+    function sendPost(){
         var palabra =  $('#search-field').val();
         var entidad =  $('#entidad-select').val();
         var fecha =  $('#date-selected').val();
@@ -35,6 +48,14 @@
                     $('#result').html(data.info);
                 }
         });
+    }
+
+    $('#search-filtro').on('click',function() {
+        sendPost();
+    });
+    
+    $('#search-field').pressEnter(function() {
+        sendPost();
     })
 JS;
     $this->registerJs('var ajaxurl = "' .Url::to(['site/filtrar-busqueda']). '";', \yii\web\View::POS_HEAD);
@@ -42,57 +63,59 @@ JS;
 ?>
 
 <div class="box box-info box-header search_strong" style="padding-top: 0px" >
-    <div class="col-xs-3 form-group">
-        <div class="box-footer">
-            <div class="input-group">
-                    <input id="search-field" type="text" name="q" class="form-control" placeholder="Buscar..."/>
-                    <span class="input-group-btn">
+   
+        <div class="col-xs-3 form-group">
+            <div class="box-footer">
+                <div class="input-group">
+                        <input id="search-field" type="text" name="q" class="form-control" placeholder="Buscar..." value="<?php if (!empty($searchFeld)){ echo $searchFeld;}?>"/>
+                        <span class="input-group-btn">
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-xs-3">
-        <div class="box-footer">
-            <select id="entidad-select" class="form-control" data-live-search="true">
-                    <option value="1">Todos</option>
-                    <option value="2">Pacientes</option>
-                    <option value="3">Protocolos</option>
-                    <option value="4">Medicos</option>
-            </select>
+        <div class="col-xs-3">
+            <div class="box-footer">
+                <select id="entidad-select" class="form-control" data-live-search="true">
+                        <option value="1">Todos</option>
+                        <option value="2">Pacientes</option>
+                        <option value="3">Protocolos</option>
+                        <option value="4">Medicos</option>
+                </select>
+            </div>
         </div>
-    </div>
-    <div class="col-xs-3 protocolo-search" style="display: none; ">
-        <div class="box-footer">
-            <?php
-                echo DateControl::widget([
-                    'name'=>'kartik-date',
-                   // 'value'=>time(),
-                    'id' => 'date-selected',
-                    'type'=>DateControl::FORMAT_DATE,
-                    'displayFormat' => 'php:d/m/Y',
-                    'saveFormat' => 'php:U',
-                ]);
-            ?>
-        </div>
-    </div>
-    <div class="col-xs-3 protocolo-search" style="display: none; ">
-        <div class="box-footer">
-            <select id="informe-filter" class="form-group form-control" data-live-search="true">
+        <div class="col-xs-3 protocolo-search" style="display: none; ">
+            <div class="box-footer">
                 <?php
-                $estudios = Estudio::find()->all();
-                foreach ($estudios as $estudio){
-                    $id = $estudio->id;
+                    echo DateControl::widget([
+                        'name'=>'kartik-date',
+                       // 'value'=>time(),
+                        'id' => 'date-selected',
+                        'type'=>DateControl::FORMAT_DATE,
+                        'displayFormat' => 'php:d/m/Y',
+                        'saveFormat' => 'php:U',
+                    ]);
                 ?>
-                    
-                    <option value="<?php echo $id;?>"><?php echo $estudio->nombre ?></option>
-                    <?php
-                }
-                ?>
-            </select>
+            </div>
         </div>
-    </div>
-    <div class="box-footer pull-right">
-        <button id="search-filtro" class="btn btn-primary">Buscar</button>
-    </div>
+        <div class="col-xs-3 protocolo-search" style="display: none; ">
+            <div class="box-footer">
+                <select id="informe-filter" class="form-group form-control" data-live-search="true">
+                    <?php
+                    $estudios = Estudio::find()->all();
+                    foreach ($estudios as $estudio){
+                        $id = $estudio->id;
+                    ?>
+                        
+                        <option value="<?php echo $id;?>"><?php echo $estudio->nombre ?></option>
+                        <?php
+                    }
+                    ?>
+                </select>
+            </div>
+        </div>
+        <div class="box-footer pull-right">
+            <button id="search-filtro" class="btn btn-primary">Buscar</button>
+        </div>
+  
 </div>
 <div id="result">
     

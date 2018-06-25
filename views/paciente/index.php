c5f15eb461775a4ffcfb3d74b3770e4483705d7e
root <franco.e.aller@gmail.com>
2018-06-25 19:42:45 +0000

    paciente/index fix delete paciente

--- views/paciente/index.php
+++ views/paciente/index.php
@@ -14,12 +14,45 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <?php
     $js = <<<JS
-     
+    $('.deletePaciente').on('click',function() {
+         var ajaxurl = $(this).attr('value');
+         $.get( ajaxurl , function( data ) {
+                if (data.rta == 'ok'){
+                        $.pjax.reload({container: '#pacientes'})
+                        console.log("entre2");
+                        var n = noty({
+                                text: data.message,
+                                type: 'success',
+                                class: 'animated pulse',
+                                layout: 'topRight',
+                                theme: 'relax',
+                                timeout: 3000, // delay for closing event. Set false for sticky notifications
+                                force: false, // adds notification to the beginning of queue when set to true
+                                modal: false, // si pongo true me hace el efecto de pantalla gris
+                                killer : true,
+                        });
+                }else{
+                        var n = noty({
+                                text: data.message,
+                                type: 'alert',
+                                class: 'animated pulse',
+                                layout: 'topRight',
+                                theme: 'relax',
+                                timeout: 3000, // delay for closing event. Set false for sticky notifications
+                                force: false, // adds notification to the beginning of queue when set to true
+                                modal: false, // si pongo true me hace el efecto de pantalla gris
+                                killer : true
+                        });
+                }
+        });
+    })
+    
      $(document).on('ready pjax:success', function () {
          $('.deletePaciente').on('click',function() {
              var ajaxurl = $(this).attr('value');
              $.get( ajaxurl , function( data ) {
-                 if (data.rta == 'ok'){ 
+                 if (data.rta == 'ok'){
+                     console.log("entre");
 					 $.pjax.reload({container: '#pacientes'})
                      var n = noty({
                             text: data.message,

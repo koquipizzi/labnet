f5615c124aafc1584955f0b31bbd7e09960717ff
Franco Aller <franco.e.aller@gmail.com>
2018-12-21 15:29:53 -0300

    migraciones de full text search

---
+++ migrations/m181218_162300_set_columns_with_full_text_search.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181218_162300_set_columns_with_full_text_search
+ */
+class m181218_162300_set_columns_with_full_text_search extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->execute(
+            "
+            PACIENTE_INDEX on Paciente(nombre,email);
+
+            CREATE fulltext index MEDICO_INDEX on Medico(nombre,email);
+            
+            CREATE fulltext index INFORME_INDEX on Informe(material,tecnica,macroscopia,microscopia,diagnostico);
+       
+            "
+       );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        return true;
+    
+    }
+
+}

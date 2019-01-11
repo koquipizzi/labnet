8bc31a0aad79ba129d6d6d09ac9a8db560737827
Franco Aller <franco.e.aller@gmail.com>
2019-01-11 15:40:26 -0300

    pdf reducido pap: texto default en caso de no tener leocusitos ni hematies

--- views/informe/print_pap_reducido.php
+++ views/informe/print_pap_reducido.php
@@ -80,8 +80,8 @@ use yii\helpers\Html;
                 <tr><td class="pap_labels_cito">CALIDAD DE MUESTRA</td> <td  class="pap_desc_cito"><?php echo $model->calidad ? Leyenda::findOne(['codigo' => $model->calidad, 'categoria' => 'C' ])->texto : ""  ?></td></tr>
                 <tr><td class="pap_labels_cito">ASPECTO</td>            <td  class="pap_desc_cito"><?php echo $model->aspecto ? Leyenda::findOne(['codigo' => $model->aspecto , 'categoria' => 'A' ] )->texto : ""  ?></td></tr>
                 <tr><td class="pap_labels_cito">FLORA</td>              <td  class="pap_desc_cito"><?php echo $model->flora ? Leyenda::findOne(['codigo' => $model->flora, 'categoria' => 'F' ])->texto : ""  ?></td></tr>
-                <tr><td class="pap_labels_cito">LEUCOCITOS</td>         <td  class="pap_desc_cito"><?php echo $model->leucositos ? Leyenda::findOne(['categoria' => 'LH','codigo'=> $model->leucositos])->texto : ""?></td></tr>
-                <tr><td class="pap_labels_cito">HEMATIES</td>           <td  class="pap_desc_cito"><?php echo $model->hematies ? Leyenda::findOne(['categoria' => 'LH','codigo'=> $model->hematies])->texto : "" ?></td></tr>
+                <tr><td class="pap_labels_cito">LEUCOCITOS</td>         <td  class="pap_desc_cito"><?php echo $model->leucositos ? Leyenda::findOne(['categoria' => 'LH','codigo'=>  $model->leucositos])->texto : "No se observan."?></td></tr>
+                <tr><td class="pap_labels_cito">HEMATIES</td>           <td  class="pap_desc_cito"><?php echo $model->hematies ? Leyenda::findOne(['categoria' => 'LH','codigo'=> $model->hematies])->texto : "No se observan." ?></td></tr>
                 <tr><td class="pap_labels_cito">OTROS ELEMENTOS</td>    <td  class="pap_desc_cito"><?php echo $model->otros ? Leyenda::findOne(['codigo' => $model->otros , 'categoria' => 'O' ])->texto : ""  ?></td></tr>
                 <tr><td class="pap_labels_cito">MICROORGANISMOS</td>    <td  class="pap_desc_cito"><?php echo $model->microorganismos ? Leyenda::findOne(['codigo' => $model->microorganismos, 'categoria' => 'M' ])->texto : ""  ?></td></tr>
             </table>

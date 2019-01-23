5b12fb03658a45d0989d48812be7cd1d727fddea
Franco Aller <franco.e.aller@gmail.com>
2019-01-23 11:28:07 -0300

    vista index: arreglo error con path relativo

--- web/index.php
+++ web/index.php
@@ -1,9 +1,16 @@
 <?php
-
 // comment out the following two lines when deployed to production
 //defined('YII_DEBUG') or define('YII_DEBUG', true);
+ 
+defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
-
+if(YII_DEBUG){
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+    ini_set("xdebug.var_display_max_children", -1);
+    ini_set("xdebug.var_display_max_data", -1);
+    ini_set("xdebug.var_display_max_depth", -1);    
+}   
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
